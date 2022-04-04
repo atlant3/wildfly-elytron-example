855ec2bf813cb616ab88c3c6f6c58f19d49fc510
M_Bilozir <bilizir2552@gmail.com>
2022-04-04 11:27:50 +0300

    Init commit

--- src/main/java/ua/lv/mb/controller/LogoutServlet.java
+++ src/main/java/ua/lv/mb/controller/LogoutServlet.java
@@ -1,8 +1,5 @@
 package ua.lv.mb.controller;
 
-import javax.management.MBeanServer;
-import javax.management.MBeanServerFactory;
-import javax.management.ObjectName;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
@@ -24,22 +21,10 @@ public class LogoutServlet extends HttpServlet {
         response.setHeader("Expires", new java.util.Date().toString());
 
         if (request.getSession(false) != null) {
-            //    clearCache(request.getRemoteUser());
-            request.getSession(false).invalidate();// remove session.
-
+            request.getSession(false).invalidate();
         }
 
         request.logout();
         response.sendRedirect(request.getContextPath());
     }
-    public void clearCache(String username){
-        try {
-            ObjectName jaasMgr = new ObjectName("jboss.as:subsystem=security,security-domain=<YOUR SECURITY DOMAIN>" );
-            Object[] params = {username};
-            String[] signature = {"java.lang.String"};
-            MBeanServer server = (MBeanServer) MBeanServerFactory.findMBeanServer(null).get(0);
-            server.invoke(jaasMgr, "flushCache", params, signature);
-        } catch (Exception ex) {
-
-        }}
 }
